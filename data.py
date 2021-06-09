4a3ea61da6e28389bd70f2a6dd43929b7d15b484
gordeykomark <47080295+gordeykomark@users.noreply.github.com>
2021-06-09 15:57:24 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,11 @@
+from main_menu import main_menu
+# Размеры поля
+BLOCK_SIZE = 30
+
+#Цвета клеток
+L_GREEN = (134, 228, 20)
+D_GREEN = (129, 217, 19)
+RED = (181, 55, 55, 128)
+SCORE_BROWN = (95, 60, 29)
+#Цвет бэкграунда
+BACKGOUND = (158, 218, 219)
